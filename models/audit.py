286d5b3c8a011024fdc6e99cbc3607e612ad0be2
Gal Radia <gal.radia14@gmail.com>
2025-01-27 12:44:34 +0200

    try fix audit id again

--- models/audit.py
+++ models/audit.py
@@ -1,30 +1,32 @@
-import uuid
+from bson import ObjectId
 from datetime import datetime
-
 import pytz
 
-
 class Audit:
-    def __init__(self, username, role, timestamp=None,_id=None):
-        self._id = str(_id) if _id else None  # Convert ObjectId to string
+    def __init__(self, username, role, timestamp=None, _id=None):
+        # Use MongoDB's ObjectId directly for the id field
+        self.id = _id if _id else None  # Use the _id as the MongoDB ObjectId
         self.username = username
         self.role = role
         self.timestamp = timestamp or datetime.now(pytz.utc)
 
     def to_dict(self):
         return {
-            '_id': self._id,
+            'id': str(self.id),  # Convert the ObjectId to string if needed
             'username': self.username,
             'timestamp': self.timestamp,
             'role': self.role
         }
+
     def __str__(self):
         return f"{self.username} {self.role} {self.timestamp}"
+
     @staticmethod
     def from_dict(data):
+        # Return the Audit object using MongoDB's ObjectId
         return Audit(
             username=data.get('username'),
             role=data.get('role'),
             timestamp=data.get('timestamp'),
-            _id=data.get('_id')  # Ensure the id is captured from MongoDB
+            _id=ObjectId(data.get('_id')) if data.get('_id') else None  # Parse the _id as ObjectId
         )
